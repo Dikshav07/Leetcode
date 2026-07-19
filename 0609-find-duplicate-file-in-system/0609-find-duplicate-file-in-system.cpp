9eaa191728249166be0d2aaada0e012e9e3c3cc0
Diksha Verma <150647851+Dikshav07@users.noreply.github.com>
2026-07-20 00:39:16 +0530

    Time: 47 ms (62.19%), Space: 40.1 MB (72.39%) - LeetHub

--- 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.cpp
+++ 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
-    vector<vector<string>> findDuplicate(vector<string>& paths) {
-        unordered_map<string, vector<string>> mp;
-        for (string &s : paths) {
-            stringstream ss(s);
-            string dir;
-            ss >> dir;
-            string file;
-            while (ss >> file) {
-                int l = file.find('(');
-                int r = file.find(')');
-
-                string name = file.substr(0, l);
-                string content = file.substr(l + 1, r - l - 1);
-
-                mp[content].push_back(dir + "/" + name);
+    vector<vector<string>> findDuplicate(vector<string>& paths) {        
+        unordered_map<string, vector<string>>mp;
+        vector<vector<string>> dup_group;
+        
+        for(const auto &x : paths) {
+            string path = "", filePath, fileName, content;
+            
+            for(int i = 0; i < x.size(); i++) {
+                if(x[i] == ' ') {
+                    i++;
+                    fileName = "";
+                    
+                    while(x[i] != '(') {
+                        fileName += x[i];
+                        i++;
+                    }
+                    
+                    i++;
+                    content = "";
+                    
+                    while(x[i] != ')') {                        
+                        content += x[i];
+                        i++;
+                    }
+                    
+                    filePath = path + '/' + fileName;
+                    mp[content].push_back(filePath);
+                }            
+                else path += x[i];
+            }            
+        }
+        
+        for(const auto &x : mp) {
+            if(x.second.size() > 1) {
+                dup_group.push_back(x.second);
             }
         }
-
-        vector<vector<string>> ans;
-
-        for (auto &x : mp)
-            if (x.second.size() > 1)
-                ans.push_back(x.second);
-
-        return ans;
+        
+        return dup_group;
+        
     }
 };
