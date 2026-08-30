9ba6d7a8b2c2e789426c75eadb283e8cbc8e2a9a
Diksha Verma <150647851+Dikshav07@users.noreply.github.com>
2026-08-30 22:28:18 +0530

    Time: 0 ms (100%), Space: 92.1 MB (58.82%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -4,10 +4,11 @@ public:
         int n = nums.size();
         int mn = 0, mx = 0;
         for (int i = 1; i < n; i++) {
-            if (nums[i] < nums[mn]) mn = i;
-            if (nums[i] > nums[mx]) mx = i;
+            if (nums[i]<nums[mn]) mn = i;
+            if (nums[i]>nums[mx]) mx = i;
         }
-        if (mn > mx) swap(mn, mx);
-        return min({mx + 1, n - mn, mn + 1 + n - mx});
+        if (mn>mx) swap(mn,mx);
+        return min({mx+1, n-mn, mn + 1 + n-mx});
     }
 };
+
