210067a9818d198c96ecae4bd7f146ac9dbf1e05
Diksha Verma <150647851+Dikshav07@users.noreply.github.com>
2026-09-07 00:20:33 +0530

    Time: 2694 ms (7.25%), Space: 412.7 MB (7.99%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        vector<vector<int>> pos(240);
-        vector<int> dp(m+1,0);
-        vector<int> dp_max(240,0);
-        for(int i=0; i<m; i++) pos[t[i]].push_back(i);
-        dp[0]=1;
-        for(int i=0; i<n; i++)
-        {
-            int c = s[i];
-            int end = pos[c].size();
-            if(end==0) continue;
-            if(dp_max[c] < end && dp[pos[c][dp_max[c]]]) dp_max[c]++;
-            
-            for(int j=dp_max[c]-1; j>=0; j--)
-            {
-                long long x = (long long) dp[pos[c][j]+1] + (long long)dp[pos[c][j]];
-                if (x <= INT_MAX) dp[pos[c][j]+1] = x;
-            } 
+    int dp[1001][1001];
+    int solve(string s,string t,int i,int j){
+        if( j>=t.size()) return 1;
+        if(i>=s.size()) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
 
+        if(s[i]==t[j]){
+            return dp[i][j]=solve(s,t,i+1,j+1)+solve(s,t,i+1,j);
         }
-        return dp[m];
+        return dp[i][j]=solve(s,t,i+1,j);
+    }
+    int numDistinct(string s, string t) {
+        memset(dp,-1,sizeof(dp));
+        return solve(s,t,0,0);
     }
 };
